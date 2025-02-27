c71505980832bb3c36a1cbad2a188e740aaba8e7
Pickky03 <vhai02092003@gmail.com>
2025-02-27 15:59:06 +0700

    Premium User

--- src/app/(public)/[locale]/exhibitions/components/gallery.tsx
+++ src/app/(public)/[locale]/exhibitions/components/gallery.tsx
@@ -2,6 +2,7 @@
 import { Canvas } from '@react-three/fiber';
 import { Loader } from './gallery-loader';
 import dynamic from 'next/dynamic';
+import { Stats } from '@react-three/drei';
 
 export default function Gallery() {
 	const Scene = dynamic(() => import('./scene').then((mod) => mod.default), {
@@ -13,12 +14,12 @@ export default function Gallery() {
 		<div className='w-full h-screen'>
 			<Canvas
 				//  shadows
-				//  dpr={[1, 2]} // Limit max DPR to 2
-				//  performance={{
-				//    min: 0.5, // Render at minimum 50% resolution
-				//    max: 1,   // Maximum 100% resolution
-				//    debounce: 200 // Wait 200ms before adjusting quality
-				//  }}
+				 dpr={[1, 1]} // Limit max DPR to 2
+				 performance={{
+				   min: 0.5, // Render at minimum 50% resolution
+				   max: 1,   // Maximum 100% resolution
+				   debounce: 200 // Wait 200ms before adjusting quality
+				 }}
 				gl={{
 					antialias: false, // Disable antialiasing for performance
 					powerPreference: 'high-performance',
@@ -27,6 +28,7 @@ export default function Gallery() {
 					depth: true
 				}}
 			>
+				<Stats/>
 				<Scene />
 			</Canvas>
 		</div>
---
+++ src/components/ui.custom/Avatar.tsx
@@ -0,0 +1,36 @@
+import { PremiumBadge } from './PremiumBadge';
+
+interface AvatarProps {
+  user: {
+    image?: string;
+    googleImage?: string;
+    isPremium: boolean;
+  };
+  size?: 'sm' | 'md' | 'lg';
+}
+
+export const Avatar = ({ user, size = 'md' }: AvatarProps) => {
+  const sizeClasses = {
+    sm: 'w-8 h-8',
+    md: 'w-12 h-12',
+    lg: 'w-16 h-16'
+  };
+
+  const avatarSrc = user.image || user.googleImage || 'https://res.cloudinary.com/djvlldzih/image/upload/v1739204028/gallery/arts/occjr92oqgbd5gyzljvb.jpg';
+
+  return (
+    <div className="relative inline-block">
+      <img
+        src={avatarSrc}
+        alt="Avatar"
+        className={`rounded-full ${sizeClasses[size]}`}
+      />
+
+      {user.isPremium && (
+        <div className="absolute -top-1 -right-1">
+          <PremiumBadge />
+        </div>
+      )}
+    </div>
+  );
+};
