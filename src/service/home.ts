35cbba61cc13e736139d39036a03edde80d1c207
bluefishhh93 <xuanthanha03@gmail.com>
2025-05-04 07:41:58 +0700

    feat: Update FeaturedSection to fetch and display featured exhibitions with improved data handling and localization; enhance API for fetching exhibitions

--- src/service/home.ts
+++ src/service/home.ts
@@ -32,10 +32,11 @@ export async function getNewExhibitions(): Promise<ApiResponse<GetPublicExhibiti
     return response;
 }
 
-export async function getFeaturedExhibitions(): Promise<ApiResponse<GetPublicExhibitionsResponse>> {
+export async function getFeaturedExhibitions(limit: number): Promise<ApiResponse<GetPublicExhibitionsResponse>> {
     const response = await getPublicExhibitions({
         filter: { isFeatured: true },
-        limit: 10
+        sort: { createdAt: -1 },
+        limit
     });
     return response;
 }
