21c7ceb24aa58f351cadd6657ec2724edbbb989f
bluefishhh93 <xuanthanha03@gmail.com>
2025-05-07 02:16:02 +0700

    refactor: Enhance artwork fetching with pagination and improve date formatting functions

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -15,10 +15,10 @@ export function vietnameseDateFormat(date: Date | string) {
 		year: 'numeric',
 		month: '2-digit',
 		day: '2-digit',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
-		hour12: false,
+		// hour: '2-digit',
+		// minute: '2-digit',
+		// second: '2-digit',
+		// hour12: false,
 		timeZone: 'Asia/Ho_Chi_Minh',
 	};
 	const dateObj = new Date(date);
@@ -30,10 +30,10 @@ export const englishDateFormat = (date: Date | string) => {
 		year: 'numeric',
 		month: '2-digit',
 		day: '2-digit',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
-		hour12: false,
+		// hour: '2-digit',
+		// minute: '2-digit',
+		// second: '2-digit',
+		// hour12: false,
 		timeZone: 'America/New_York', // or 'UTC' depending on your need
 	};
 	const dateObj = new Date(date);
